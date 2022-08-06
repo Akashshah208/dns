f877857b581d6cbeecf7bb8e9ee9d5c3e50b9813
parth <mr.parth.developer@gmail.com>
2022-08-06 19:23:09 +0530

    - Blog Table Create And Model Create

---
+++ database/migrations/2022_08_06_135133_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('tag');
+            $table->string('profile');
+            $table->longText('description');
+            $table->unsignedBigInteger('auth_id')->unsigned();
+            $table->enum('status', ['0', '1'])->default('1');
+            $table->timestamps();
+            $table->foreign('auth_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
